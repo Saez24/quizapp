1218f5a62ec71c7df0b917e164f98a1049fda754
Alex <saez0724+github@googlemail.com>
2024-04-07 17:59:45 +0200

    finished js

--- script/script.js
+++ script/script.js
@@ -56,3 +56,100 @@ let questions = [
         "right_answer": 4
     }
 ];
+
+let rightQuestions = 0;
+let currentQuestion = 0;
+let AUDIO_FAIL = new Audio('./sounds/wrong.mp3');
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+
+    showQuestion();
+};
+
+function showQuestion() {
+    if (gameIsOver()) {
+        showEndscreen();
+
+    } else { // Show next question
+        updateToNextQuestion();
+    }
+};
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    if (selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success'); // parentNode steht für das übergeordnete Element, in diesem Fall wird der Übergeordnten div der id="answer_1 oder 2 oder 3 oder 4" die css class hinzugefügt.
+        rightQuestions++;
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
+    }
+    document.getElementById('next-button').disabled = false;
+
+};
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons()
+    showQuestion();
+};
+
+function resetAnswerButtons() {
+
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+};
+
+function restartGame() {
+    document.getElementById('header-image').src = './img/school.jpeg'; //Bild wird wieder zurückgesetzt
+    document.getElementById('endScreen').style = 'display: none'; //Endscreen div wird wieder ausgeblendet
+    document.getElementById('questionBody').style = ''; // Questionbody wird wieder eingeblendet
+    rightQuestions = 0;
+    currentQuestion = 0;
+    init();
+};
+
+function showEndscreen() {
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none';
+    document.getElementById('amount-of-questions').innerHTML = questions.length;
+    document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+    document.getElementById('header-image').src = './img/trophy.png';
+};
+
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+
+}
+
+function updateToNextQuestion() {
+    updateProgressbar();
+
+    let question = questions[currentQuestion];
+    document.getElementById('question-number').innerHTML = currentQuestion + 1;
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+};
+
+function updateProgressbar() {
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progress-bar').innerHTML = `${percent}%`;
+    document.getElementById('progress-bar').style.width = `${percent}%`;
+}
